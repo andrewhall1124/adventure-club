4db8931ccee81dec42f758f3ce96fcdcd670a44c
Andrew Hall <andrewmartinhall2@gmail.com>
2023-07-15 13:21:52 -0600

    Home, Products, Product-handle

--- app/components/ProductGallery.jsx
+++ app/components/ProductGallery.jsx
@@ -10,42 +10,38 @@ export function ProductGallery({media, className}) {
 
   return (
     <div
-      className={`swimlane md:grid-flow-row hiddenScroll md:p-0 md:overflow-x-auto md:grid-cols-2 ${className}`}
+      className={"carousel h-[62vh] sm:h-[70vh] lg:h-[90vh] w-full overflow-scroll py-6 pl-6"}
     >
-      {media.map((med, i) => {
-        const isFirst = i === 0;
-        const isFourth = i === 3;
-        const isFullWidth = i % 3 === 0;
+      <div className="flex h-full w-full sm:w-[50%]">
+        {media.map((med, i) => {
+          const isFirst = i === 0;
+          const isFourth = i === 3;
+          const isFullWidth = i % 3 === 0;
 
-        const image =
-          med.__typename === 'MediaImage'
-            ? {...med.image, altText: med.alt || 'Product image'}
-            : null;
+          const image =
+            med.__typename === 'MediaImage'
+              ? {...med.image, altText: med.alt || 'Product image'}
+              : null;
 
-        const style = [
-          isFullWidth ? 'md:col-span-2' : 'md:col-span-1',
-          isFirst || isFourth ? '' : 'md:aspect-[4/5]',
-          'aspect-square snap-center card-image bg-white dark:bg-contrast/10 w-mobileGallery md:w-full',
-        ].join(' ');
-
-        return (
-          <div className={style} key={med.id || image?.id}>
-            {image && (
-              <Image
-                loading={i === 0 ? 'eager' : 'lazy'}
-                data={image}
-                aspectRatio={!isFirst && !isFourth ? '4/5' : undefined}
-                sizes={
-                  isFirst || isFourth
-                    ? '(min-width: 48em) 60vw, 90vw'
-                    : '(min-width: 48em) 30vw, 90vw'
-                }
-                className="object-cover w-full h-full aspect-square fadeIn"
-              />
-            )}
-          </div>
-        );
-      })}
+          return (
+            <div className='h-full w-full flex-grow flex-shrink-0 pr-6'  key={med.id || image?.id}>
+              {image && (
+                  <Image
+                    loading={i === 0 ? 'eager' : 'lazy'}
+                    data={image}
+                    // aspectRatio={!isFirst && !isFourth ? '4/5' : undefined}
+                    // sizes={
+                    //   isFirst || isFourth
+                    //     ? '(min-width: 48em) 60vw, 90vw'
+                    //     : '(min-width: 48em) 30vw, 90vw'
+                    // }
+                    className="h-full w-full object-cover my-shadow"
+                  />
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
